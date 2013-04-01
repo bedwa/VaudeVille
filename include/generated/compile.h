089941e1c20c329a3a4a1aa4bbe9c71c65e3ca41
Bedwa <bedwablackburn@gmail.com>
2013-03-31 23:47:18 -0500

    JB kernel 3.16 p4

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Mar 31 16:06:52 CDT 2013"
+#define LINUX_COMPILE_BY "bedwa"
+#define LINUX_COMPILE_HOST "Bedwa-LT40"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
