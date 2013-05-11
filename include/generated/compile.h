e091d3c5fad51148aa2930e22abe217a91f5c062
Bedwa <bedwablackburn@gmail.com>
2013-05-11 15:59:58 -0500

    modified:   .version 	modified:   include/config/kernel.release 	modified:   include/generated/compile.h 	modified:   include/generated/utsrelease.h 	modified:   sound/soc/codecs/sound_control_gpl.c

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Wed Apr 3 23:51:10 CDT 2013"
+#define UTS_VERSION "#2 SMP PREEMPT Sat May 11 11:42:56 CDT 2013"
 #define LINUX_COMPILE_BY "bedwa"
 #define LINUX_COMPILE_HOST "Bedwa-LT40"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.4.10"
+#define UTS_RELEASE "3.4.10-g6b55c87-dirty"
